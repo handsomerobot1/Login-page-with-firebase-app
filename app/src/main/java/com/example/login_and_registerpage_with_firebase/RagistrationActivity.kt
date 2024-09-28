b495a47853a38f576ef65a1e8a2c604aed44e5da
HandsomeRobot <alaminjoy456@gmail.com>
2024-09-28 12:57:34 +0600

    layout degin done

---
+++ app/src/main/java/com/example/login_and_registerpage_with_firebase/RagistrationActivity.kt
@@ -0,0 +1,15 @@
+package com.example.login_and_registerpage_with_firebase
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RagistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ragistration)
+
+    }
+}
